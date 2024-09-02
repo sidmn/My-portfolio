4b2ce544833a44d14e6fcb53f18270441149dbc1
Siddharth Mantri <76742878+sidmn@users.noreply.github.com>
2024-09-03 05:12:05 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
-import {withSentryConfig} from '@sentry/nextjs';
-/** @type {import('next').NextConfig} */
+
+ @type {import('next').NextConfig} 
 const nextConfig = {
     output: 'export',
     typescript: {
