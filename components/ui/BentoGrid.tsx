223c27975ab510f76ac4b6fdb2371c50a242fc61
sidmn <sidmantri3@gmail.com>
2024-09-03 14:16:05 +0530

    removed grid Globe

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
-import GridGlobe from "./GridGlobe";
 import { useState } from "react";
 import Lottie from "react-lottie";
 import animationData from '@/data/confetti.json'
@@ -116,8 +115,6 @@ export const BentoGridItem = ({
           >
             {title}
           </div>
-
-          {id=== 2 && <GridGlobe />}
 
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
