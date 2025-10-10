b37f3fa0a376e0b40a53b67c0383fa09fc9eefd7
sidmn <sidmantri3@gmail.com>
2025-10-11 00:17:52 +0530

    updated to vercel link

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
             Hi, I&apos;m Siddharth Mantri, a Frontend Developer Based in Kolkata, India.            
           </p>
 
-          <a href="https://scrapbook.siddharth-mantri.co.in/">
+          <a href="cool-portfolio-eta.vercel.app">
             <MagicButton 
               title="Visit my Scrapbook"
               icon={<FaLocationArrow />}
